73597b651d1d1db8a9778379d472fa17c2bae521
Quy <tranphuquy19@gmail.com>
2020-03-10 22:35:00 +0700

    v-for bug!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const readExcelFile = require('read-excel-file/node');
 const fs = require('fs');
 const _ = require('lodash');
-const {xlsDir} = require('../configs');
+const { xlsDir } = require('../configs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -23,58 +23,84 @@ router.post('/upload', (req, res, next) => {
     if (err)
       return res.status(500).json(err);
 
-    res.status(201).json({message: 'Upload successful'})
+    res.status(201).json({ message: 'Upload successful' })
   });
 });
 
-router.get('/generate', (req, res, next) => {
+router.post('/generate', (req, res, next) => {
   let { xlsFile, filters, sheet, isGenerate } = req.body;
+  if (fs.existsSync(`${xlsDir}/${xlsFile}.xlsx`)) {
+    readExcelFile(fs.createReadStream(`${xlsDir}/${xlsFile}.xlsx`), { sheet: sheet | 1 }).then(data => {
+      data.splice(0, 1);
+      let temp = [];
+      data.map(e => temp.push({
+        id: e[0],
+        lesson: e[1],
+        stt: e[2],
+        tl: e[3],
+        category: e[4],
+        name: e[5],
+        kanji: e[6],
+        hanViet: e[7],
+        meaning: e[8]
+      }));
 
-  readExcelFile(fs.createReadStream(`D:/generate-quiz/public/xls/${xlsFile}.xlsx`), { sheet: sheet | 1 }).then(data => {
-    data.splice(0, 1);
-    let temp = [];
-    data.map(e => temp.push({
-      id: e[0],
-      lesson: e[1],
-      stt: e[2],
-      tl: e[3],
-      category: e[4],
-      name: e[5],
-      kanji: e[6],
-      hanViet: e[7],
-      meaning: e[8]
-    }));
-
-    if (filters) {
-      Object.keys(filters).map(e => {
-        temp = temp.filter(t => {
-          return t[e] === filters[e];
+      if (filters) {
+        Object.keys(filters).map(e => {
+          temp = temp.filter(t => {
+            return t[e] === filters[e];
+          });
         });
-      });
-    }
-    if (isGenerate) {
-      let generated = [];
-      generated = temp.map(e => {
-        let otherAns = temp.filter(f => { return e.name !== f.name });
+      }
+      if (isGenerate) {
+        let generated = [];
+        generated = temp.map(e => {
+          let otherAns = temp.filter(f => { return e.name !== f.name });
 
-        otherAns = _.shuffle(otherAns);
+          otherAns = _.shuffle(otherAns);
 
-        let j = { ...e };
-        j.a = e.name;
-        j.b = otherAns.pop().name;
-        j.c = otherAns.pop().name;
-        j.d = otherAns.pop().name;
-        return j;
-      });
-      res.json(generated);
-    } else {
-      res.json(temp);
-    }
-  });
+          let j = { ...e };
+          j.a = e.name;
+          j.b = otherAns.pop().name;
+          j.c = otherAns.pop().name;
+          j.d = otherAns.pop().name;
+          return j;
+        });
+        res.json(generated);
+      } else {
+        res.json(temp);
+      }
+    });
+  } else {
+    res.status(404).json({ message: 'File not found' });
+  }
+
 });
 
-router.get('/metadata', (req, res, next) => {
-  let { xlsFile, filters, sheet, isGenerate } = req.body;
+router.post('/metadata', (req, res, next) => {
+  let { xlsFile, sheet } = req.body;
+  if (fs.existsSync(`${xlsDir}/${xlsFile}.xlsx`)) {
+    readExcelFile(fs.createReadStream(`${xlsDir}/${xlsFile}.xlsx`), { sheet: sheet | 1 }).then(data => {
+      data.splice(0, 1);
+      let lesson = new Set();
+      let tl = new Set();
+      let category = new Set();
+
+      data.map(e => {
+        if(e[1]) lesson.add(e[1]);
+        if(e[3]) tl.add(e[3]);
+        if(e[4]) category.add(e[4]);
+      });
+
+      let opts = {
+        lesson: Array.from(lesson),
+        tl: Array.from(tl),
+        category: Array.from(category)
+      };
+
+      res.status(200).json(opts);
+    })
+  } else { res.status(404).json({ message: 'File not found' }); };
 });
 
 module.exports = router;
