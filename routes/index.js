692c6e42ab767290b1e15b3be21e8cc1d546a67d
Quy <tranphuquy19@gmail.com>
2020-03-12 19:05:52 +0700

    done code

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { xlsDir } = require('../configs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
- res.redirect('/index.html');
+  res.redirect('/index.html');
 });
 
 router.post('/upload', (req, res, next) => {
@@ -25,6 +25,114 @@ router.post('/upload', (req, res, next) => {
 
     res.status(201).json({ message: 'Upload successful' })
   });
+});
+
+router.post('/kanji', (req, res, next) => {
+  let { xlsFile, sheet, isGenerate, partionLesson } = req.body;
+  console.log(req.body);
+  if (fs.existsSync(`${xlsDir}/${xlsFile}.xlsx`)) {
+    readExcelFile(fs.createReadStream(`${xlsDir}/${xlsFile}.xlsx`), { sheet: sheet | 1 }).then(data => {
+      data.splice(0, 1);
+      let temp = [];
+      let id = 0;
+      let kanjiLessons = new Set();
+      data.map(e => temp.push({
+        id: id++,
+        kanji: e[0],
+        lesson: e[1],
+        hanViet: e[2],
+        kanjiGoi: e[3], //question
+        pronounce: e[4], //answer
+        meaning: e[5]
+      }));
+
+
+      if (partionLesson) {
+        let temp2 = [];
+        if (partionLesson.trim().includes('*')) {
+          temp.map(e => {
+            kanjiLessons.add(e.lesson);
+            temp2.push(e);
+          });
+        } else {
+          let lessons = partionLesson.replace(' ', '').split(',');
+          lessons.map(le => {
+            temp.map(te => {
+              if (String(le).includes('-')) {
+                let [m, n] = le.split('-');
+                if (te.lesson >= m && te.lesson <= n) {
+                  kanjiLessons.add(te.lesson); temp2.push(te);
+                }
+              } else {
+                if (te.lesson == le) {
+                  kanjiLessons.add(te.lesson); temp2.push(te);
+                }
+              }
+            })
+          });
+        }
+        temp = temp2;
+      }
+
+      // Array.from(kanjiLessons).map(k => console.log(k));
+      // temp.map(e => console.log(e));
+      // if (filters) {
+      //   Object.keys(filters).map(e => {
+      //     temp = temp.filter(t => {
+      //       return t[e] === filters[e];
+      //     });
+      //   });
+      // }
+
+      if (isGenerate) {
+        let generated = [];
+        Array.from(kanjiLessons).map(kl => {
+          let kjTemp = [], kjAnsTemp = []; // const of this session
+
+          temp.map(t => {
+            if (t.lesson == kl) {
+              kjTemp.push(t);
+              kjAnsTemp.push(t.pronounce);
+            };
+          }); // end temp
+
+          let ansPop = [];
+
+          kjTemp.map(e => {
+            let ansTemp_ = kjAnsTemp.filter(a => {
+              if (a !== e.pronounce) ansPop.push(a);
+            });
+            ansTemp_ = _.shuffle(ansTemp_);
+            let ansPart = [];
+
+            ansPart.push({ ans: e.pronounce, check: true });
+            ansPart.push({ ans: ansPop.pop(), check: false });
+            ansPart.push({ ans: ansPop.pop(), check: false });
+            ansPart.push({ ans: ansPop.pop(), check: false });
+
+            ansPart = _.shuffle(ansPart);
+
+            generated.push({
+              ...e,
+              a: ansPart[0].ans,
+              aCheck: ansPart[0].check,
+              b: ansPart[1].ans,
+              bCheck: ansPart[1].check,
+              c: ansPart[2].ans,
+              cCheck: ansPart[2].check,
+              d: ansPart[3].ans,
+              dCheck: ansPart[3].check
+            })
+          }); // end kjTemp
+        }); //end kanjiLession
+        res.json(generated);
+      } else {
+        res.json(temp);
+      }
+    });
+  } else {
+    res.status(404).json({ message: 'File not found' });
+  }
 });
 
 router.post('/generate', (req, res, next) => {
@@ -108,6 +216,14 @@ router.post('/generate', (req, res, next) => {
     res.status(404).json({ message: 'File not found' });
   }
 
+});
+
+router.post('/kanjimetadata', (req, res, next) => {
+  let { xlsFile, sheet } = req.body;
+  if (fs.existsSync(`${xlsDir}/${xlsFile}.xlsx`)) {
+    res.status(200).json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'File not found' })
 });
 
 router.post('/metadata', (req, res, next) => {
